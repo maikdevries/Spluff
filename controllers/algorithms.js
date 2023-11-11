7b69cc9a1b9b969f09021f6647aa1588a9ad42e8
maikdevries <dev.maik.devries@outlook.com>
2023-11-11 13:35:17 +0100

    feat: implement Durstenfeld shuffle algorithm

---
+++ controllers/algorithms.js
@@ -0,0 +1,14 @@
+module.exports = {
+	shuffle,
+}
+
+// NOTE: Durstenfeld variant of the Fisher-Yates shuffle algorithm
+function shuffle (array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+
+	return array;
+}
