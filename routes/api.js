bcdae18885b383c7caa6c61055626a01abec489c
maikdevries <dev.maik.devries@outlook.com>
2023-11-11 17:03:50 +0100

    feat: implement API /shuffle endpoint

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { shuffle } = require('../controllers/algorithms.js');
+const { addPlaylistItems, deletePlaylistItems, getPlaylistItems } = require('../controllers/spotify.js');
 
 module.exports = router;
 
@@ -17,5 +19,21 @@ router.use('/', (req, res, next) => {
 });
 
 router.get('/playlists/:playlistID/shuffle', async (req, res, next) => {
-	res.send(`NOT IMPLEMENTED: API /playlists/${req.params.playlistID}/shuffle GET`);
+	try {
+		const items = await getPlaylistItems(req.session, req.params.playlistID);
+
+		// NOTE: If there are no items to process, return early with 'NO CONTENT' status
+		if (!items.length) return res.status(204).json({ 'description': 'This request resulted in no content changes' });
+
+		// NOTE: Remove all items from original playlist so they can be re-added in shuffled order
+		await deletePlaylistItems(req.session, req.params.playlistID, items);
+
+		// NOTE: Re-add all items to original playlist in shuffled order
+		await addPlaylistItems(req.session, req.params.playlistID, shuffle(items.map((x) => x.uri)));
+
+		return res.json({ 'description': 'This request was completed successfully' });
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ 'error': 'Something went terribly wrong on our side of the internet' });
+	}
 });
