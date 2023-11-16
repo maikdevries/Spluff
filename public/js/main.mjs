b2b672de7efc3a3bb3c76ab21bdaefb836462d36
maikdevries <dev.maik.devries@outlook.com>
2023-11-16 14:44:17 +0100

    feat: implement user profile dropdown logic

---
+++ public/js/main.mjs
@@ -0,0 +1,9 @@
+document.getElementById('userData')?.addEventListener('click', (event) => showUserDropdown(event), {
+	'capture': true,
+	'once': false,
+	'passive': true,
+});
+
+function showUserDropdown (event) {
+	return event.currentTarget.nextElementSibling.classList.toggle('invisible');
+}
