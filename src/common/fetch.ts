9bcf52066f2f307c20e3a3444d4cf2d4e0bba14f
maikdevries <dev.maik.devries@outlook.com>
2025-01-09 18:21:08 +0100

    feat: implement fetch JSON utility function

--- src/common/fetch.ts
+++ src/common/fetch.ts
@@ -1,8 +1,28 @@
-import type { HTTP_METHOD } from './types.ts';
+import type { HTTP_METHOD, JSON } from './types.ts';
 
 class FetchError extends Error {
 	constructor(status: number, method: keyof typeof HTTP_METHOD, url: string) {
 		super(`Fetch request failed with status ${status}. URL: ${method} ${url}`);
 		this.name = this.constructor.name;
 	}
+}
+
+async function json(method: keyof typeof HTTP_METHOD, url: URL, headers: HeadersInit, body: BodyInit): Promise<JSON> | never {
+	let response: Response;
+
+	try {
+		response = await fetch(url, {
+			'method': method,
+			'headers': {
+				'Accept': 'application/json',
+				...headers,
+			},
+			'body': body,
+		});
+	} catch {
+		throw new FetchError(503, method, url.toString());
+	}
+
+	if (response.ok) return await response.json() as JSON;
+	else throw new FetchError(response.status, method, response.url);
 }
--- src/common/types.ts
+++ src/common/types.ts
@@ -2,3 +2,7 @@ export const HTTP_METHOD = {
 	GET: 'GET',
 	POST: 'POST',
 } as const;
+
+export interface JSON {
+	[key: string]: string | number | boolean | null | JSON | JSON[];
+}
