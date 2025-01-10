b21ebfcf39b59bc8a6cbb1d795e765e0d19c409e
maikdevries <dev.maik.devries@outlook.com>
2025-01-10 12:01:15 +0100

    feat: implement authorisation service

---
+++ src/services/auth.services.ts
@@ -0,0 +1,37 @@
+import type { Credentials } from '../common/types.ts';
+
+import * as fetch from '../common/fetch.ts';
+
+export async function retrieve(code: string, verifier: string): Promise<Credentials> {
+	const params = new URLSearchParams({
+		'grant_type': 'authorization_code',
+		'code': code,
+		'redirect_uri': Deno.env.get('SPOTIFY_REDIRECT_URI') ?? '',
+		'client_id': Deno.env.get('SPOTIFY_CLIENT_ID') ?? '',
+		'code_verifier': verifier,
+	});
+
+	const data = await fetch.auth(params);
+
+	return {
+		'token': data.access_token,
+		'expires': Date.now() + data.expires_in * 1000,
+		'refresh': data.refresh_token as string,
+	};
+}
+
+export async function refresh(token: string): Promise<Credentials> {
+	const params = new URLSearchParams({
+		'grant_type': 'refresh_token',
+		'refresh_token': token,
+		'client_id': Deno.env.get('SPOTIFY_CLIENT_ID') ?? '',
+	});
+
+	const data = await fetch.auth(params);
+
+	return {
+		'token': data.access_token,
+		'expires': Date.now() + data.expires_in * 1000,
+		'refresh': data.refresh_token ?? token,
+	};
+}
