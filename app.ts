5ab6bf5b5fd69e3b20f500a4febc05690575368c
maikdevries <dev.maik.devries@outlook.com>
2025-03-29 16:38:30 +0100

    fix: change HTTP server hostname to 127.0.0.1

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import router from './src/router.ts';
 
 Deno.serve({
+	'hostname': '127.0.0.1',
 	'port': Number.parseInt(Deno.env.get('DENO_PORT') ?? ''),
 }, async (request: Request) => await router(request));
