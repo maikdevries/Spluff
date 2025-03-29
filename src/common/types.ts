7f6587fe3e7b5ed935b038940bb1f31ea417377b
maikdevries <dev.maik.devries@outlook.com>
2025-03-29 18:50:25 +0100

    feat: define playlist type interface

--- src/common/types.ts
+++ src/common/types.ts
@@ -9,6 +9,12 @@ export const HTTP_METHOD = {
 	'POST': 'POST',
 } as const;
 
+interface Image {
+	'height': number | null;
+	'url': string;
+	'width': number | null;
+}
+
 export interface JSON {
 	[key: string]: string | number | boolean | null | JSON | JSON[];
 }
@@ -28,10 +34,42 @@ export interface PKCE {
 	'verifier': string;
 }
 
+export interface Playlist {
+	'collaborative': boolean;
+	'description': string;
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'id': string;
+	'images': Image[];
+	'name': string;
+	'owner': User;
+	'public': boolean;
+	'snapshot_id': string;
+	'tracks': {
+		'href': string;
+		'total': number;
+	};
+	'type': 'playlist';
+	'uri': string;
+}
+
 export interface TokenResponse {
 	'access_token': string;
 	'token_type': 'Bearer';
 	'scope': string;
 	'expires_in': number;
 	'refresh_token': string | undefined;
+}
+
+interface User {
+	'display_name': string | null;
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'id': string;
+	'type': 'user';
+	'uri': string;
 }
