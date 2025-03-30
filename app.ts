b216496cb7dbd2d802fae69df82da386c7492787
maikdevries <dev.maik.devries@outlook.com>
2025-03-30 16:42:36 +0200

    revert fix: change HTTP server hostname to 127.0.0.1
    This reverts commit 5ab6bf5b5fd69e3b20f500a4febc05690575368c.

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import router from './src/router.ts';
 
 Deno.serve({
-	'hostname': '127.0.0.1',
 	'port': Number.parseInt(Deno.env.get('DENO_PORT') ?? ''),
 }, async (request: Request) => await router(request));
