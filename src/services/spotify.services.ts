17459c2b9a237a00eb3476b447f9f79b8269054a
maikdevries <dev.maik.devries@outlook.com>
2025-03-30 21:32:18 +0200

    feat: support playlist tracks spotify service method

--- src/services/spotify.services.ts
+++ src/services/spotify.services.ts
@@ -1,7 +1,11 @@
-import type { Playlist } from '../common/types.ts';
+import type { Playlist, PlaylistTrack } from '../common/types.ts';
 
 import * as fetch from '../common/fetch.ts';
 
 export async function playlists(token: string): Promise<Playlist[]> {
 	return await fetch.items<Playlist>(token, 'GET', 'me/playlists');
+}
+
+export async function playlistTracks(token: string, id: string): Promise<PlaylistTrack[]> {
+	return await fetch.items<PlaylistTrack>(token, 'GET', `playlists/${id}/tracks`);
 }
