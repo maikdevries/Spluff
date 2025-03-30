a00a3bf93d4c34e267046e5f9991de988125497f
maikdevries <dev.maik.devries@outlook.com>
2025-03-30 21:16:58 +0200

    feat: define playlist track type interface

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,7 +1,62 @@
+interface BaseAlbum {
+	'album_type': 'album' | 'compilation' | 'single';
+	'artists': BaseArtist[];
+	'available_markets': string[];
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'id': string;
+	'images': Image[];
+	'name': string;
+	'release_date': string;
+	'release_date_precision': 'day' | 'month' | 'year';
+	'total_tracks': number;
+	'type': 'album';
+	'uri': string;
+}
+
+interface BaseArtist {
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'id': string;
+	'name': string;
+	'type': 'artist';
+	'uri': string;
+}
+
+interface Copyright {
+	'text': string;
+	'type': 'C' | 'P';
+}
+
 export interface Credentials {
 	'token': string;
 	'expires': number;
 	'refresh': string;
+}
+
+interface Episode {
+	'description': string;
+	'duration_ms': number;
+	'explicit': boolean;
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'html_description': string;
+	'id': string;
+	'images': Image[];
+	'is_externally_hosted': boolean;
+	'languages': string[];
+	'name': string;
+	'release_date': string;
+	'release_date_precision': string;
+	'show': Show;
+	'type': 'episode';
+	'uri': string;
 }
 
 export const HTTP_METHOD = {
@@ -61,6 +116,60 @@ export interface TokenResponse {
 	'scope': string;
 	'expires_in': number;
 	'refresh_token': string | undefined;
+}
+
+export interface PlaylistTrack {
+	'added_at': string | null;
+	'added_by': BaseUser | null;
+	'is_local': boolean;
+	'track': Episode | Track;
+}
+
+interface Show {
+	'available_markets': string[];
+	'copyrights': Copyright[];
+	'description': string;
+	'explicit': boolean;
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'html_description': string;
+	'id': string;
+	'images': Image[];
+	'is_externally_hosted': boolean;
+	'languages': string[];
+	'media_type': string;
+	'name': string;
+	'publisher': string;
+	'total_episodes': number;
+	'type': 'show';
+	'uri': string;
+}
+
+interface Track {
+	'album': BaseAlbum;
+	'artists': BaseArtist[];
+	'available_markets': string[];
+	'disc_number': number;
+	'duration_ms': number;
+	'explicit': boolean;
+	'external_ids': {
+		'isrc': string;
+		'ean': string;
+		'upc': string;
+	};
+	'external_urls': {
+		'spotify': string;
+	};
+	'href': string;
+	'id': string;
+	'is_local': boolean;
+	'name': string;
+	'popularity': number;
+	'track_number': number;
+	'type': 'track';
+	'uri': string;
 }
 
 interface BaseUser {
